f23091f56e394d959cc507c7fe17e62fbf6edb1f
“renatowillon” <renatowillon@hotmail.com>
2025-02-11 14:03:40 -0300

    feat: +1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,9 +9,21 @@ const Home = () => {
       <Banner />
       <Separator />
       <div className="grid grid-cols-3 h-36 text-center items-center">
-        <div>conteudo em 3 grids, conteudo a definir</div>
-        <div className="border-x-2">conteudo em 3 grids, conteudo a definir</div>
-        <div>conteudo em 3 grids, conteudo a definir</div>
+        <div className="p-4">
+          <p>Icone</p>
+          <p>Creative Learning</p>
+          <p>Your child has numerous ways to learn,the gamified way!.</p>
+        </div>
+        <div className="border-x-2 p-4">
+          <p>Icone</p>
+          <p>Cognitive Development</p>
+          <p>Boost your child’s cognitive development with several hands-on activities.</p>
+        </div>
+        <div className="p-4">
+          <p>ICone</p>
+          <p>Various Activities</p>
+          <p>Several diverse activities in a box to explore!</p>
+        </div>
       </div>
       <Separator />
     </div>
